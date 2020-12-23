e4fe09aa3980a57d10785a3e3cff67151faa3188
Matheus Roxo Brandão <matheus.roxo.brandao31@gmail.com>
2020-12-23 01:01:23 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AulaPOO_PolimosrfismoSobreposicao.classes;
+
+namespace AulaPOO_PolimosrfismoSobreposicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MegaMan jogador1 = new MegaMan();
+
+            Console.WriteLine($"Jogador 1 = {jogador1.Correr()} e {jogador1.Pular()}");
+
+            Zero jogador2 = new Zero();
+
+            Console.WriteLine($"Jogador 2 = {jogador2.Correr()} e {jogador2.Pular()}");
+        }
+    }
+}
